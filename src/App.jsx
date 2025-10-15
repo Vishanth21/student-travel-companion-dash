0ca1bf276040a3ce8f3e61653d0e3bf0f553a475
Vishanth <81638898+Vishanth21@users.noreply.github.com>
2025-10-15 19:03:47 +0530

    adds travel media component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import TravelMedia from "./TravelMedia.jsx";
+
+function App() {
+  return (
+    <div className="main-dash">
+      <h1>Smart Travel Companion Dashboard</h1>
+      <div className="cards">
+        <TravelMedia/>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/TravelMedia.jsx
@@ -0,0 +1,16 @@
+import styles from "./TravelMedia.module.css";
+
+function TravelMedia() {
+    const videoUrl = "https://getsamplefiles.com/download/mp4/sample-3.mp4";
+    const audioUrl = "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3";
+
+    return (
+        <div className={styles['travel-media-body']}>
+            <h1 className={styles.header}>Travel Media</h1>
+            <video className={styles.video} src={videoUrl} controls preload="auto">Your browser does not support the video tag.</video>
+            <audio className={styles.audio} src={audioUrl} controls preload="auto">Your browser does not support the audio tag.</audio>
+        </div>
+    )
+}
+
+export default TravelMedia;
